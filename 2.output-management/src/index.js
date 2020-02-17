e59700a2b3b71655e1c06fd63835bba97f6e5399
baljnyam <baljnyam@gmail.com>
2020-02-17 11:57:00 +1100

    .

--- 2.output-management/src/index.js
+++ 2.output-management/src/index.js
@@ -1,23 +1,9 @@
 import _ from "lodash";
-import "./style.css";
-import Icon from "./icon.png";
-import Data from "./data.xml";
 
 function component() {
   const element = document.createElement("div");
 
-  // Lodash, now imported by this script
-  // style.css imported
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
 
   return element;
 }
