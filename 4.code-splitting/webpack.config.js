2c5a37be00a122aaaa80f9e93ff5d161777a3ad0
baljnyam <baljnyam@gmail.com>
2020-02-17 16:02:00 +1100

    .

--- 4.code-splitting/webpack.config.js
+++ 4.code-splitting/webpack.config.js
@@ -3,14 +3,9 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: {
     app: "./src/index.js",
     print: "./src/print.js"
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: "./dist"
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -20,7 +15,6 @@ module.exports = {
   ],
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/"
+    path: path.resolve(__dirname, "dist")
   }
 };
